f931ccfac47adb7010202dfe772f968b9006b08d
Unknown <shawngauvin@hotmail.com>
2019-07-31 14:28:40 -0400

    Initial Project Commit

---
+++ TeamGosuWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeamGosuWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
