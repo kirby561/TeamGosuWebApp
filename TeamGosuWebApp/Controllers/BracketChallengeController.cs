945eafb59b2814628082b5e3d13bbe780b1cd67f
Alex Hunt <alexanderthunt@gmail.com>
2024-11-12 02:07:02 -0500

    Started adding bracket challenge code. It is functional now but needs some layout cleanup and colors and things.

---
+++ TeamGosuWebApp/Controllers/BracketChallengeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TeamGosuWebApp.Models;
+using TeamGosuWebApp.Services;
+
+namespace TeamGosuWebApp.Controllers
+{
+    public class BracketChallengeController : Controller {
+        public BracketChallengeController() {
+        }
+
+        public IActionResult Pr10BracketChallenge() {
+            return View();
+        }
+
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
